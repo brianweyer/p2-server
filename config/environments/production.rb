cc1abd2a87351174a7157dfb7eae20e93b0008e1
Brian Weyer <brianweyer@gmail.com>
2017-02-18 14:45:53 -0600

    Update env dependency

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_controller.asset_host = ENV[url]
+  config.action_controller.asset_host = ENV['URL'] || "http://localhost:3000"
 end
