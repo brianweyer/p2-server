a08dc620a6c4deb8070e0b551b560cf5491231d1
Brian Weyer <brianweyer@gmail.com>
2017-02-19 22:05:27 -0600

    Update to some JS to make it work on every page load, issue caused by turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,4 @@
 //= require_tree .
 
 
-$(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
-});
+
